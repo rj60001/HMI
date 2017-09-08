3fc8ba66dee51f100408f13606cf032622338808
Reiss Jones <reiss1999@gmail.com>
2017-09-08 15:18:12 +0100

    Changed from PHP to HTML

--- js/entryScript.js
+++ js/entryScript.js
@@ -37,70 +37,55 @@ function menuPopUp(enterBtn, bgObjs, bgObjCon, menu, body) {
 }
 
 function setColorScheme(time){
-  objectNameList = ["title", "subTitle", "button"];
-  objectTagList = ["fieldset", "input", "select", "textarea"];
-  finalList = [];
-  finalNumList = [];
-  for(i = 0; i < objectNameList.length; i++) {
-    array = document.getElementsByName(objectNameList[i]);
-    console.log(array);
-    finalList = finalList.concat(array);
-    finalNumList.push(array.length);
-  }
-  for(i = 0; i < objectTagList.length; i++) {
-    array = document.getElementsByTagName(objectTagList[i]);
-    finalList = finalList.concat(array);
-    finalNumList.push(array.length);
-  }
-  finalTotalNum = 0;
-  for(i = 0; i < finalNumList.length; i++) {
-    finalTotalNum += finalNumList[i];
-  }
-  console.log(finalList);
-  console.log(finalNumList);
   if(time === "day") {
-    for(i = 0; i < finalTotalNum; i++) {
-      if(i <= finalNumList[0]){
-        finalList[i][i].style.color = "#FFB71C";
-      }
-      else if(i <= finalNumList[1] && i > finalNumList[0]){
-        finalList[i].style.color = "#FEFEFE";
-      }
-      else if(i <= finalNumList[2] && i > finalNumList[1]){
-        finalList[i].style.color = "#EFEFEF";
-        finalList[i].style.borderColor = "#EFEFEF";
-      }
-      else if(i <= finalNumList[3] && i > finalNumList[2]){
-        finalList[i].style.color = "#EFEFEF";
-        finalList[i].style.borderColor = "#EFEFEF";
-      }
-      else if(i <= finalNumList[4] && i > finalNumList[3]){
-        finalList[i].style.color = "#16A7E5";
-        finalList[i].style.borderColor = "#EFEFEF";
-        finalList[i].style.backgroundColor = "#EFEFEF";
-      }
-      else if(i <= finalNumList[5] && i > finalNumList[4]){
-        finalList[i].style.color = "#16A7E5";
-        finalList[i].style.borderColor = "#EFEFEF";
-        finalList[i].style.backgroundColor = "#EFEFEF";
-      }
-      else if(i <= finalNumList[6] && i > finalNumList[5]){
-        finalList[i].style.color = "#16A7E5";
-        finalList[i].style.borderColor = "#EFEFEF";
-        finalList[i].style.backgroundColor = "#EFEFEF";
-      }
+    body.style.background = "linear-gradient(45deg, #17EA29, #16A7E5, #17EA29)";
+    body.style.backgroundSize = "400% 400%";
+    body.style.backgroundPosition = "0% 100%";
+    body.style.color = "#FEFEFE";
+    
+    objects = document.getElementsByName("title");
+    for(i = 0; i < objects.length; i++){
+      objects[i].style.color = "#FFB71C";
     }
+    objects = document.getElementsByName("subTitle");
+    for(i = 0; i < objects.length; i++){
+      objects[i].style.color = "#FEFEFE";
+    }
+    objects = document.getElementsByTagName("fieldset");
+    for(i = 0; i < objects.length; i++){
+      objects[i].style.color = "#EFEFEF";
+      objects[i].style.borderColor = "#EFEFEF";
+    }
+    objects = document.getElementsByName("button");
+    for(i = 0; i < objects.length; i++){
+      objects[i].style.color = "#EFEFEF";
+        objects[i].style.borderColor = "#EFEFEF";
+    }
+    objects = document.getElementsByTagName("input");
+    for(i = 0; i < objects.length; i++){
+      objects[i].style.color = "#16A7E5";
+      objects[i].style.borderColor = "#EFEFEF";
+      objects[i].style.backgroundColor = "#EFEFEF";
+    }
+    objects = document.getElementsByTagName("textarea");
+    for(i = 0; i < objects.length; i++){
+      objects[i].style.color = "#16A7E5";
+      objects[i].style.borderColor = "#EFEFEF";
+      objects[i].style.backgroundColor = "#EFEFEF";
+    }
+    objects = document.getElementsByTagName("select");
+    for(i = 0; i < objects.length; i++){
+      objects[i].style.color = "#16A7E5";
+      objects[i].style.borderColor = "#EFEFEF";
+      objects[i].style.backgroundColor = "#EFEFEF";
+    }
+
+    css = ".button:hover { background-color: #EFEFEF; color: #16A7E5 !important; }";
+    style = document.createElement("style");
+    style.appendChild(document.createTextNode(css));
+    document.getElementsByTagName("head")[0].appendChild(style);
   }
   else if(time === "night"){
-    /*w*/
-  }
-}
-
-
-function dayChangeClick(){
-  body = document.getElementsByTagName("body")[0];
-  btn = document.getElementById("dayChangeBtn");
-  if(btn.getAttribute("info") === "day") {
     body.style.background = "#333";
     body.style.color = "#777";
     objects = document.getElementsByName("title");
@@ -143,19 +128,20 @@ function dayChangeClick(){
     style = document.createElement("style");
     style.appendChild(document.createTextNode(css));
     document.getElementsByTagName("head")[0].appendChild(style);
+  }
+}
+
+
+function dayChangeClick(){
+  body = document.getElementsByTagName("body")[0];
+  btn = document.getElementById("dayChangeBtn");
+  if(btn.getAttribute("info") === "day") {
+    setColorScheme("night");
     btn.setAttribute("info", "night");
     btn.innerHTML = '<i class="material-icons">brightness_1</i>';
   }
   else if(btn.getAttribute("info") !== "day") {
-    body.style.background = "linear-gradient(45deg, #17EA29, #16A7E5, #17EA29)";
-    body.style.backgroundSize = "400% 400%";
-    body.style.backgroundPosition = "0% 100%";
-    body.style.color = "#FEFEFE";
     setColorScheme("day");
-    css = ".button:hover { background-color: #EFEFEF; color: #16A7E5 !important; }";
-    style = document.createElement("style");
-    style.appendChild(document.createTextNode(css));
-    document.getElementsByTagName("head")[0].appendChild(style);
     btn.setAttribute("info", "day");
     btn.innerHTML = '<i class="material-icons">brightness_3</i>';
   }
--- js/inputScript.js
+++ js/inputScript.js
@@ -1,7 +1,20 @@
 function dnaInputCheck(element){
   value = element.value.slice[-1];
-  if (value !== "A" || value !== "G" || value !== "T" || value !== "C") {
+  if (value != "A" && value !== "G" && value != "T" && value != "C") {
     console.log("poo");
-    element.value.slice(0, element.value.length-1);
+    element.value = element.value.substr(0, element.value.length-1);
+  }
+}
+
+function clearValue(value){
+  if(value.value == value.getAttribute("info")){
+    value.value = "";
+  }
+}
+
+function restoreValue(value){
+  if(value.value == ""){
+    x = value.getAttribute("info");
+    value.value = x;
   }
 }
