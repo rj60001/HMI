ae3970b4a09d47c6faf43711d231e93ccdf3a6fe
Reiss Jones <reiss1999@gmail.com>
2017-09-19 10:56:18 +0100

    DNA input check change.

--- js/inputScript.js
+++ js/inputScript.js
@@ -1,6 +1,6 @@
 function dnaInputCheck(element){
   value = element.value.slice[-1];
-  if (value != "A" && value !== "G" && value != "T" && value != "C") {
+  if (value !== "A" && value !== "G" && value !== "T" && value !== "C") {
     console.log("poo");
     element.value = element.value.substr(0, element.value.length-1);
   }
