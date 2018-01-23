68d1bb6eaf6c41086f7ac9362dec57b265a93a53
rj60001 <reiss1999@gmail.com>
2018-01-23 07:41:02 +0000

    Error checfking for editing a sequence

--- php/submission/toolSingleView.php
+++ php/submission/toolSingleView.php
@@ -3,27 +3,36 @@
     $nucleosome = $trim["nucleosomeTE"];
     $mods = $trim["histoneModsTE"];
     $dna = strtoupper($trim["dnaSequenceTE"]);
-    $q = "";
-    if($nucleosome == "NULL"){ # This means that we need to create a new record.
+    $errors = [];
+
+    if(strlen($dna) != 127){
+      $errors = ["The DNA sequence does not fill the nucelosome's capacity (127bp)."]
+    }
+
+    if(empty($errors)){
+      $q = "";
       $matchResult = checkDNA($dna);
       $ndsid = -1; # Not yet assigned with an actual ID value, but needs to be decared so that it has the correct variable scope.
       if($matchResult == FALSE) {
         $r = mysqli_query($db, "INSERT INTO nucleosomednasequence VALUES(NULL, '$dna')");
       }
       $ndsid = mysqli_fetch_array(mysqli_query($db, "SELECT ndsid FROM nucleosomednasequence WHERE DNASequence='".$dna."'"))[0];
-      $q = "INSERT INTO nucleosome VALUES(NULL, $ndsid, '$mods', ".$_GET["sequence"].")";
-    }
-    else { # This means we need to update a record.
-      $matchResult = checkDNA($dna);
-      $ndsid = -1; # Not yet assigned with an actual ID value, but needs to be decared so that it has the correct variable scope.
-      if($matchResult == FALSE) {
-        $r = mysqli_query($db, "INSERT INTO nucleosomednasequence VALUES(NULL, '$dna')"); # Insert a new DNA sequence record as DNA has not been found already in the table.
+      if($nucleosome == "NULL"){ # This means that we need to create a new record.
+        $q = "INSERT INTO nucleosome VALUES(NULL, $ndsid, '$mods', ".$_GET["sequence"].")";
       }
-      $ndsid = mysqli_fetch_array(mysqli_query($db, "SELECT ndsid FROM nucleosomednasequence WHERE DNASequence='".$dna."'"))[0];
-      $q = "UPDATE nucleosome SET ndsid='$ndsid', histoneMods='$mods' WHERE nid=$nucleosome";
+      else { # This means we need to update a record.
+        $q = "UPDATE nucleosome SET ndsid='$ndsid', histoneMods='$mods' WHERE nid=$nucleosome";
+      }
+      $r = mysqli_query($db, $q);
+      echo('<script>window.location.href="index.php?page=tool&sequence='.$nsid.'";</script>');
     }
-    $r = mysqli_query($db, $q);
-    echo('<script>window.location.href="index.php?page=tool&sequence='.$nsid.'";</script>');
+    else {
+      echo($popupTop);
+      foreach($errors as $error){
+        echo('<p>'.$error.'</p>');
+      }
+      echo($popupBottom);
+    }
   }
   else if(isset($trim["nsidTD"]) && isset($_GET["disease"])){ # Editing a disease.
     $nsid = $trim["nsidTD"];
